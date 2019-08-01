6f8ab55b24035e470ffb528cd555f1a3d9015d91
AlphaCodingcode <dilshan@alphacoding.com>
2019-08-01 16:44:54 -0400

    import tileset

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 // Any global variables can be defined up here
-
+let tileNames = ["brick", "space"];
+let tileset;
+let worldMap = [];
 
 /*
     Code in the setup function will only be run once at the start of the animation
 */
+
+function preload() {
+    tileset = new Tileset("imgs/pacman_tileset.png", 32, 32, tileNames);
+}
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
 }
