744b493af2c1e7067e8817470d2e9f8ddb58435c
AlphaCodingcode <dilshan@alphacoding.com>
2019-08-01 17:04:18 -0400

    Create Pacman map

--- sketch.js
+++ sketch.js
@@ -1,18 +1,41 @@
 // Any global variables can be defined up here
 let tileNames = ["brick", "space"];
 let tileset;
-let worldMap = [];
+let worldMap = [
+["space", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "space"],
+["space", "brick", "space", "space", "space", "space", "space", "space", "space", "space", "brick", "space", "space", "space", "space", "space", "space", "space", "space", "brick", "space"],
+["space", "brick", "space", "brick", "brick", "space", "brick", "brick", "brick", "space", "brick", "space", "brick", "brick", "brick", "space", "brick", "brick", "space", "brick", "space"],
+["space", "brick", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "brick", "space"],
+["space", "brick", "space", "brick", "brick", "space", "brick", "space", "brick", "brick", "brick", "brick", "brick", "space", "brick", "space", "brick", "brick", "space", "brick", "space"],
+["space", "brick", "space", "space", "space", "space", "brick", "space", "space", "space", "brick", "space", "space", "space", "brick", "space", "space", "space", "space", "brick", "space"],
+["space", "brick", "brick", "brick", "brick", "space", "brick", "brick", "brick", "space", "brick", "space", "brick", "brick", "brick", "space", "brick", "brick", "brick", "brick", "space"],
+["space", "space", "space", "space", "brick", "space", "brick", "space", "space", "space", "space", "space", "space", "space", "brick", "space", "brick", "space", "space", "space", "space"],
+["brick", "brick", "brick", "brick", "brick", "space", "brick", "space", "brick", "brick", "space", "brick", "brick", "space", "brick", "space", "brick", "brick", "brick", "brick", "brick"],
+["space", "space", "space", "space", "space", "space", "space", "space", "brick", "space", "space", "space", "brick", "space", "space", "space", "space", "space", "space", "space", "space"],
+["brick", "brick", "brick", "brick", "brick", "space", "brick", "space", "brick", "brick", "brick", "brick", "brick", "space", "brick", "space", "brick", "brick", "brick", "brick", "brick"],
+["space", "space", "space", "space", "brick", "space", "brick", "space", "space", "space", "space", "space", "space", "space", "brick", "space", "brick", "space", "space", "space", "space"],
+["space", "brick", "brick", "brick", "brick", "space", "brick", "space", "brick", "brick", "brick", "brick", "brick", "space", "brick", "space", "brick", "brick", "brick", "brick", "space"],
+["space", "brick", "space", "space", "space", "space", "space", "space", "space", "space", "brick", "space", "space", "space", "space", "space", "space", "space", "space", "brick", "space"],
+["space", "brick", "space", "brick", "brick", "space", "brick", "brick", "brick", "space", "brick", "space", "brick", "brick", "brick", "space", "brick", "brick", "space", "brick", "space"],
+["space", "brick", "space", "space", "brick", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "brick", "space", "space", "brick", "space"],
+["space", "brick", "brick", "space", "brick", "space", "brick", "space", "brick", "brick", "brick", "brick", "brick", "space", "brick", "space", "brick", "space", "brick", "brick", "space"],
+["space", "brick", "space", "space", "space", "space", "brick", "space", "space", "space", "brick", "space", "space", "space", "brick", "space", "space", "space", "space", "brick", "space"],
+["space", "brick", "space", "brick", "brick", "brick", "brick", "brick", "brick", "space", "brick", "space", "brick", "brick", "brick", "brick", "brick", "brick", "space", "brick", "space"],
+["space", "brick", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "space", "brick", "space"],
+["space", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "brick", "space"],
+];
 
-/*
+/*sni
     Code in the setup function will only be run once at the start of the animation
 */
 
 function preload() {
     tileset = new Tileset("imgs/pacman_tileset.png", 32, 32, tileNames);
+    tileset.setMap(worldMap);
 }
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(21 * 32, 21 * 32);
 }
 
 /*
@@ -21,4 +44,15 @@ function setup() {
 function draw() {
     // Update
     // Render
+    //tileset.drawTile("brick", width / 2, height / 2);
+    //background(0);
+    tileset.displayMap();
+}
+
+function mouseClicked() {
+    tileset.handleClick();
+}
+
+function mouseDragged() {
+    tileset.handleDrag();
 }
